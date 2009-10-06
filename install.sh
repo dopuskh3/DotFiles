1494a83aba76dd838cd7765035456a4fa40bf717
F. Visconte <f.visconte@gmail.com>
2009-10-06 10:37:20 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+set -x 
+
+
+# zsh 
+function installlink(){
+    
+    [ -e ~/$2 ] && (echo "Backup existing $2";  mv ~/$2 ~/$2-$(date +"%Y%m%d%H%M%S").bak)
+    ln -sf $(readlink -f $1) ~/$2
+
+}
+
+installlink zsh/zshrc .zshrc
+installlink screen/screenrc .screenrc
+installlink vim/vim .vim
+installlink vim/vimrc .vimrc
