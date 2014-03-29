adfd04e6b9362529635f453c938f750872cd6099
Francois Visconte <f.visconte@criteo.com>
2014-03-29 18:49:24 +0100

    Zsh

--- zsh/zsh/bootstrap.zsh
+++ zsh/zsh/bootstrap.zsh
@@ -12,10 +12,13 @@ zmodload zsh/terminfo
 disable -a ls
 loadmodules
 source ~/.zsh/init.zsh
+antigen-use oh-my-zsh
 
 antigen-bundle zsh-users/zsh-syntax-highlighting
 antigen bundle kennethreitz/autoenv
 antigen bundle zsh-users/zsh-history-substring-search
+
+antigen-theme bira
 
 antigen-apply
 enable -a ls
