a92735549de110257088c58c4aaf816a50587100
Francois Visconte <f.visconte@criteo.com>
2014-07-07 10:22:19 +0200

    Fix zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
-zsh_dir=`dirname $0`/
-abs_zsh_dir=`readlink -f ${zsh_dir}`
-ln -sf ${abs_zsh_dir}/antigen/antigen.zsh ${abs_zsh_dir}/zsh/modules/
+
+zsh_dir=$(readlink -f `dirname ${BASH_SOURCE}`)
+
+ln -sf ${zsh_dir}/third-party/antigen/antigen.zsh ${zsh_dir}/zsh/modules/
