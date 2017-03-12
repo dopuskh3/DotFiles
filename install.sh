47f157b3b30c89749e57503fdf52d2c384b8b8a3
Francois Visconte <f.visconte@criteo.com>
2017-03-12 15:23:31 +0100

    Config

--- install.sh
+++ install.sh
@@ -32,6 +32,8 @@ dot-install scripts/spotify-command bin/spotify-command
 dot-install X/colors .config/Xcolors
 dot-install systemd/user .config/systemd/user
 dot-install yaourt/yaourtrc .yaourtrc
+dot-install i3 .config/i3
+dot-install dunst .config/dunst
 
 if [ -d ${dotdir}/urxvt-font-size ]; then
   pushd urxvt-font-size
